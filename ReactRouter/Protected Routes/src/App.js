bcad82334e1182d518eb971ea792bb4fedba7862
sravani <kunchasravani@gmail.com>
2025-01-21 14:31:56 +0530

    rename

---
+++ ReactRouter/Protected Routes/src/App.js
@@ -0,0 +1,66 @@
+import "./styles.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider
+} from "react-router-dom";
+
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { List } from "./pages/List";
+import { Contact } from "./pages/Contact";
+import { ItemDetails } from "./pages/ItemDetails";
+import { NotFound } from "./pages/NotFound";
+import { useState } from "react";
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  // create high-level protected route below
+  const PrivateRoute=({children})=>{
+   if(!isLoggedIn){
+    return <Home loggedIn={isLoggedIn} setLoggedin={setIsLoggedIn}/>
+   }
+   else{
+    return children;
+   }
+  } 
+
+  // protect the routes for the contact, list and item details pages
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      errorElement: <NotFound />,
+      element: <Navbar />,
+      children: [
+        {
+          index: true,
+          element: <Home loggedIn={isLoggedIn} setLoggedin={setIsLoggedIn} />
+        },
+        {
+          path: "/contact",
+          element: <PrivateRoute><Contact /></PrivateRoute>
+        },
+        {
+          path: "/list",
+          children: [
+            {
+              index: true,
+              element: <PrivateRoute><List /></PrivateRoute>
+            },
+            {
+              path: ":itemId",
+              element: <PrivateRoute><ItemDetails /></PrivateRoute>
+            }
+          ]
+        }
+      ]
+    }
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
